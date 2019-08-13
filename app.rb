706baed60f89c5096f1b5d2fb784427e897e020d
Vadim Dimnich <v.dimnich@gmail.com>
2019-08-13 16:03:18 +0300

    execute simple query

--- app.rb
+++ app.rb
@@ -1,3 +1,9 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new 'myTestDB.db'
+
+db.execute "INSERT INTO Cars (Name, Price) VALUES ('Honda', 7000)"
+
+db.close #закрываем соединение с БД, хороший тон
+
+
